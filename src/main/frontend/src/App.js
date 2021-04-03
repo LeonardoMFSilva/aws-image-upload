fa90a9b078c8d3004d904eb2fcfc54ec87730456
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-04-03 18:23:29 -0300

    Rendering User Profile in App.js

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -5,17 +5,30 @@ import axios from "axios";
 
 
 const UserProfiles = () => {
+  
+  const [UserProfiles, setUserProfile] = useState([]);
+
   const fetchUserProfiles = () => {
     axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
-
+    setUserProfile(res.data);
     });
 
   }
+
   useEffect(() => {
     fetchUserProfiles();
   }, []);
 
-  return <h1>Hello</h1>;
+  return UserProfiles.map((userProfile, index) => {
+
+    return (
+      <div key={index}>
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileID}</p>
+      </div>
+    )
+    
+  });
 }
 
 function App() {
