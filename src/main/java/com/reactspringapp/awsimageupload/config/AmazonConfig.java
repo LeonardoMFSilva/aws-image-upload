f53b9da07065d933a9e432693c28e346263eed34
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-04-04 21:12:21 -0300

    Testing random Amazon keys

--- src/main/java/com/reactspringapp/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/reactspringapp/awsimageupload/config/AmazonConfig.java
@@ -14,8 +14,8 @@ public class AmazonConfig {
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "",
-                ""
+                "colocar a chave aqui",
+                "colocar a chave aqui"
         );
         return AmazonS3ClientBuilder
                 .standard()
