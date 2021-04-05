11d5870b30ffd7e5200a3609be3a2e177c39cae7
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-04-04 21:28:57 -0300

    Adding react-dropzone

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,8 @@
-import React, {useState, userEffect, useEffect} from "react";
+import React, {useState, userEffect, useEffect, useCallback} from "react";
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import { useDropzone } from "react-dropzone";
 
 
 const UserProfiles = () => {
@@ -23,12 +24,32 @@ const UserProfiles = () => {
 
     return (
       <div key={index}>
+        <Dropzone />
         <h1>{userProfile.username}</h1>
         <p>{userProfile.userProfileID}</p>
       </div>
     )
     
   });
+};
+
+function Dropzone() {
+  const onDrop = useCallback (acceptedFiles => {
+    //do somenthing with the files
+
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone ({ onDrop })
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+        <p>Drop the files here ...</p> :
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
 }
 
 function App() {
