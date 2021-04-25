ee18a81eb5ef83824f666c4eaacd570baeb9385e
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-04-25 16:46:32 -0300

    Method checking if user exists in DB

--- src/main/java/com/reactspringapp/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/reactspringapp/awsimageupload/profile/UserProfileService.java
@@ -24,10 +24,18 @@ public class UserProfileService {
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file){
         if (file.isEmpty()) {
             throw new IllegalStateException("Cannot upload empty file [" + file.getSize() +"]");
-        }
+        } // Checking if the image is not empty
 
         if (!Arrays.asList(ContentType.IMAGE_JPEG, ContentType.IMAGE_PNG, ContentType.IMAGE_GIF).contains(file.getContentType())) {
             throw new IllegalStateException("File must be an image!");
-        }
+        } // Checking if the file is an image
+
+        UserProfile user = userProfileDataAcessService.
+                getUserProfiles()
+                .stream() //search for stream in Java
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId)) //what this does until here: goes through a list, then filters a userProfile which the id is equal to the one that came from the client
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(String.format("User profile %s not found.", userProfileId)));
     }
+
 }
