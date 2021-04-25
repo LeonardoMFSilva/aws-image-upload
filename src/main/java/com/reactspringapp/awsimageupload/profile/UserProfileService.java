86d31ffe8aeae896c18e8f2ccb29047854825470
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-04-25 16:22:56 -0300

    Implementing UserProfileImage methods

--- src/main/java/com/reactspringapp/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/reactspringapp/awsimageupload/profile/UserProfileService.java
@@ -1,9 +1,11 @@
 package com.reactspringapp.awsimageupload.profile;
 
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,6 +22,12 @@ public class UserProfileService {
     }
 
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file){
+        if (file.isEmpty()) {
+            throw new IllegalStateException("Cannot upload empty file [" + file.getSize() +"]");
+        }
 
+        if (!Arrays.asList(ContentType.IMAGE_JPEG, ContentType.IMAGE_PNG, ContentType.IMAGE_GIF).contains(file.getContentType())) {
+            throw new IllegalStateException("File must be an image!");
+        }
     }
 }
